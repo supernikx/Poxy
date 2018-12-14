642e6e41a3dd938854da8e236eda4deff3957281
Tamagna Davide <supernikx@gmail.com>
2018-12-14 10:40:21 +0100

    Fix playerSM e EnemySM

--- Assets/Script/StateMachine/Custom/EnemySM/States/EnemyParasiteState.cs
+++ Assets/Script/StateMachine/Custom/EnemySM/States/EnemyParasiteState.cs
@@ -8,6 +8,11 @@ namespace StateMachine.EnemySM
     {
         public override void Enter()
         {
+            Debug.Log("Enemy");
+
+            context.enemy.gameObject.transform.parent = context.player.transform;
+            context.enemy.gameObject.transform.localPosition = Vector3.zero;
+
             //Giusto per notare il cambio di stato nella build (da togliere)
             context.enemy.gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.blue;      
         }
--- Assets/Script/StateMachine/Custom/PlayerSM/States/PlayerParasiteState.cs
+++ Assets/Script/StateMachine/Custom/PlayerSM/States/PlayerParasiteState.cs
@@ -8,9 +8,10 @@ namespace StateMachine.PlayerSM
     {
         public override void Enter()
         {
+            Debug.Log("Player");
+
             context.player.transform.position = new Vector3(context.parasiteEnemy.gameObject.transform.position.x, context.parasiteEnemy.gameObject.transform.position.y, context.player.transform.position.z);
-            context.parasiteEnemy.gameObject.transform.parent = context.player.transform;
-            context.parasiteEnemy.gameObject.transform.localPosition = Vector3.zero;
+
             context.player.EnableGraphics(false);
             context.player.GetCollisionController().CalculateParasiteCollision(context.parasiteEnemy);
 
