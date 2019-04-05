ff061a461336806623cc916fa8fe25544c0fb23e
The091266 <andrea.nalin61@gmail.com>
2019-04-05 16:58:27 +0200

    Test Lightning LVL 3, Aggiunto VFX Stun, Parallax.

---
+++ Assets/VFX/Stun/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public AnimationCurve xVelocity, yVelocity, zVelocity;
+    float time;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        transform.Rotate(xVelocity.Evaluate(time),  yVelocity.Evaluate(time), zVelocity.Evaluate(time));
+    }
+}
