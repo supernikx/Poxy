a7d856e223461fbabf31efff94986645d1da0b56
Francesco-Musio <francescomusio1@gmail.com>
2019-05-04 12:41:21 +0200

    Falling Platform finita

--- Assets/Script/Objects/Platforms/FallingPlatform.cs
+++ Assets/Script/Objects/Platforms/FallingPlatform.cs
@@ -14,18 +14,52 @@ public class FallingPlatform : PlatformBase
     private TriggerOption triggerOption;
     [SerializeField]
     private GameObject graphic;
+
+    [Header("Collisions")]
+    [SerializeField]
+    /// <summary>
+    /// Numero di raycast orrizontali
+    /// </summary>
+    private int verticalRayCount = 4;
+    [SerializeField]
+    private LayerMask playerLayer;
+    /// <summary>
+    /// Spazio tra un raycast verticale e l'altro
+    /// </summary>
+    private float verticalRaySpacing;
+    /// <summary>
+    /// Offset del bound del collider
+    /// </summary>
+    private float collisionOffset = 0.015f;
+    /// <summary>
+    /// Referenza agli start point
+    /// </summary>
+    private RaycastStartPoints raycastStartPoints;
+
     private Collider collider;
 
     private FallingTrigger fallingTrigger;
-    
+       
     private Vector3 startingPosition;
 
+    private Coroutine respawnCoroutine = null;
+
     private bool isActive;
+    /// <summary>
+    /// Indica se nel precedente frame il player era a contatto con la piattaforma
+    /// </summary>
+    private bool previousContactState;
+    /// <summary>
+    /// Indica se nel frame corrente il player è a contatto con la piattaforma
+    /// </summary>
+    private bool currentContactState;
 
     private void Respawn()
     {
         graphic.SetActive(true);
         collider.enabled = true;
+        currentContactState = false;
+        previousContactState = false;
         isActive = true;
     }
 
@@ -33,12 +67,23 @@ public class FallingPlatform : PlatformBase
     public override void Init()
     {
         collider = GetComponent<Collider>();
+        previousContactState = false;
+        currentContactState = true;
+        fallingTrigger = GetComponentInChildren<FallingTrigger>();
 
         if (triggerOption == TriggerOption.BeforeTouch)
         {
-            fallingTrigger = GetComponentInChildren<FallingTrigger>();
             fallingTrigger.FallTriggerEvent += HandleFallTriggerEvent;
         }
+        else if (triggerOption == TriggerOption.AfterTouch)
+        {
+            fallingTrigger.gameObject.SetActive(false);
+
+            CalculateRaySpacing();
+            StartCoroutine(CCollisionCheck());
+        }
+
+        LevelManager.OnPlayerDeath += HandleOnPlayerDeath;
 
         isActive = true;
     }
@@ -52,8 +97,14 @@ public class FallingPlatform : PlatformBase
             isActive = false;
             graphic.SetActive(false);
             collider.enabled = false;
-            StartCoroutine(CRespawn());
+            respawnCoroutine = StartCoroutine(CRespawn());
         }
+    }
+
+    private void HandleOnPlayerDeath()
+    {
+        StopCoroutine(respawnCoroutine);
+        Respawn();
     }
     #endregion
 
@@ -72,22 +123,114 @@ public class FallingPlatform : PlatformBase
             Respawn();
         }
     }
-    #endregion
 
-    private void OnCollisionExit(Collision collision)
+    private IEnumerator CCollisionCheck()
     {
-        if (isActive && collision.gameObject.tag == "Player" && triggerOption == TriggerOption.AfterTouch)
+        while (true)
         {
-            isActive = false;
-            graphic.SetActive(false);
-            StartCoroutine(CRespawn());
+            if (isActive)
+            {
+                UpdateRaycastOrigins();
+
+                currentContactState = false;
+                VerticalCollisions();
+
+                if (!currentContactState && currentContactState != previousContactState)
+                {
+                    HandleFallTriggerEvent();
+                }
+                else
+                {
+                    previousContactState = currentContactState;
+                }
+            }
+
+            yield return null;
         }
     }
+    #endregion
+
+    #region Collision
+    /// <summary>
+    /// Funzione che calcola lo spazio tra i raycast sia verticali che orrizontali
+    /// </summary>
+    private void CalculateRaySpacing()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(collisionOffset * -2);
+
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+        
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+
+    }
+
+    /// <summary>
+    /// Funzione che calcola i 2 punti principali da cui partono i raycast
+    /// AltoDestra, AltoSinistra, BassoDestra, BassoSinistra
+    /// </summary>
+    private void UpdateRaycastOrigins()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(collisionOffset * -2);
+
+        raycastStartPoints.upperLeft = new Vector3(bounds.min.x, bounds.max.y, transform.position.z);
+        raycastStartPoints.upperRight = new Vector3(bounds.max.x, bounds.max.y, transform.position.z);
+
+    }
+
+    /// <summary>
+    /// Funzione che controlla se avviene una collisione sugli assi verticali
+    /// </summary>
+    private void VerticalCollisions()
+    {
+        //Rileva la direzione in cui si sta andando
+        float directionY = 1;
+        //Determina la lunghezza del raycast
+        float rayLenght = 0.3f;
+
+        //Cicla tutti i punti da cui deve partire un raycast sull'asse verticale
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            //Determina il punto da cui deve partire il ray
+            Vector3 rayOrigin = raycastStartPoints.upperLeft;
+            rayOrigin += transform.right * (verticalRaySpacing * i);
+
+            //Crea il ray
+            Ray ray = new Ray(rayOrigin, transform.up * directionY);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, rayLenght, playerLayer))
+            {
+                //Se colpisco qualcosa sul layer di collisione azzero la velocity
+                rayLenght = hit.distance;
+
+                Player _player;
+                if (hit.transform.gameObject.tag == "Player")
+                    _player = hit.transform.gameObject.GetComponent<Player>();
+                else
+                    _player = hit.transform.gameObject.GetComponentInParent<Player>();
+
+                currentContactState = currentContactState || (_player != null);
+            }
+            Debug.DrawRay(rayOrigin, transform.up * directionY * rayLenght, Color.red);
+        }
+    }
+    #endregion
+    
+    #region Classes
+    public enum TriggerOption
+    {
+        BeforeTouch,
+        AfterTouch,
+    }
+
+    private struct RaycastStartPoints
+    {
+        public Vector3 upperLeft;
+        public Vector3 upperRight;
+    }
+    #endregion
 
 }
 
-public enum TriggerOption
-{
-    BeforeTouch,
-    AfterTouch,
-}
