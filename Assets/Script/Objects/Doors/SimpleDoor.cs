fa42f07c07a68a5af2a169b3ec181a0e0d27ff38
Francesco-Musio <francescomusio1@gmail.com>
2019-05-17 12:10:02 +0200

    unexpected Hodor

--- Assets/Script/Objects/Doors/SimpleDoor.cs
+++ Assets/Script/Objects/Doors/SimpleDoor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using DG.Tweening;
 
 public class SimpleDoor : DoorBase
 {
@@ -8,34 +9,41 @@ public class SimpleDoor : DoorBase
     [SerializeField]
     private bool isActive;
 
+    [Header("Animation Settings")]
+    [SerializeField]
+    private Transform finalPosition;
+    [SerializeField]
+    private float duration;
+
     private bool currentState;
+    private Vector3 startingPosition;
 
     #region API
     public override void Init()
     {
         //ActivateEvent += HandleActivate;
+
+        startingPosition = transform.position;
         Setup();
     }
 
     public override void Setup()
     {
+        transform.position = startingPosition;
         currentState = isActive;
         gameObject.SetActive(currentState);
     }
 
     public override void Activate()
     {
-        currentState = !currentState;
-        this.gameObject.SetActive(currentState);
+        transform.DOMoveY(finalPosition.position.y, duration);
     }
     #endregion
 
     #region Handlers
-    /*
-    private void HandleActivate(object sender, EventArgs e)
-    {
-        Debug.Log("Activate");
-    }
-    */
+    //private void HandleActivate(object sender, EventArgs e)
+    //{
+    //    Debug.Log("Activate");
+    //}
     #endregion
 }
