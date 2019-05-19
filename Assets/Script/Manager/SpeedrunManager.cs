4f5fef7cd94ec919d55544830b6e7ac920416383
Francesco-Musio <francescomusio1@gmail.com>
2019-05-19 16:47:47 +0200

    Speedrun

---
+++ Assets/Script/Manager/SpeedrunManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SpeedrunManager : MonoBehaviour
+{
+    #region Delegates
+    public static Action StartTimer;
+    public static Action StopTimer;
+    #endregion
+    
+    [SerializeField]
+    private float timer;
+
+    private bool canCount;
+
+    #region API
+    public void Init(bool _isActive)
+    {
+        StartTimer += HandleStartTimer;
+        StopTimer += HandleStopTimer;
+    }
+    #endregion
+
+    #region Coroutines
+    private IEnumerator CTimerUpdate()
+    {
+        while (canCount)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+    #endregion
+
+    #region Handlers
+    private void HandleStartTimer()
+    {
+        timer = 0;
+        canCount = true;
+        StopAllCoroutines();
+        StartCoroutine(CTimerUpdate());
+    }
+
+    private void HandleStopTimer()
+    {
+        canCount = false;
+        StopAllCoroutines();
+        // timer is score
+    }
+    #endregion
+
+    private void OnDisable()
+    {
+        StartTimer -= HandleStartTimer;
+        StopTimer -= HandleStopTimer;
+    }
+}
